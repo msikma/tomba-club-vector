097b08d6bbce8ea52b100c85eaac5b12a5a6c28a
Michiel Sikma <michiel@sikma.org>
2025-01-09 23:35:05 +0100

    Add --watch option

--- build.sh
+++ build.sh
@@ -2,10 +2,15 @@
 
 SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 
-if ! command -v sass 2>&1 >/dev/null
-then
+if ! command -v sass 2>&1 >/dev/null; then
   echo "error: Install SassC 1.43.1 or up to build."
   exit 1
 fi
 
-sass "$SCRIPT_DIR/scss/tc-vector.scss":"$SCRIPT_DIR/TombaClub/tc-vector.css"
+if [[ "$1" == "--watch" ]]; then
+  WATCH_OPTION="--watch"
+else
+  WATCH_OPTION=""
+fi
+
+sass $WATCH_OPTION "$SCRIPT_DIR/scss/tc-vector.scss":"$SCRIPT_DIR/TombaClub/tc-vector.css"
